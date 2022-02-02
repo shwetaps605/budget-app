a993b454adc8e4fcdfb903e496810a6951336260
shwetaps605 <shwetaroy305@gmail.com>
2022-02-02 23:56:25 +0530

    Added delete buttons in view expenses modal

--- src/components/view-expenses-modal/view-expenses-modal.component.jsx
+++ src/components/view-expenses-modal/view-expenses-modal.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useBudgets } from '../../contexts/BudgetContext'
 import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import { Stack } from 'react-bootstrap'
 
 
 export default function ViewExpensesModal({ show, handleClose, budgetId }) {
@@ -13,14 +15,18 @@ export default function ViewExpensesModal({ show, handleClose, budgetId }) {
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header>
-                <Modal.Title>Expenses</Modal.Title>
+                <Stack direction='horizontal' gap={3} className='mb-4'>
+                    <Modal.Title className='me-auto'>Expenses</Modal.Title>
+                    <Button variant='danger'>Delete</Button>
+                </Stack>
             </Modal.Header>
             <Modal.Body>
                 {
                     expenses.map(expense => (
-                        <div key={expense.id} className='d-flex justify-content-space-between mb-3'>
+                        <div key={expense.id} className='mb-3'>
                             <h3>{expense.description}</h3>
                             <p>{expense.amount}</p>
+                            <Button variant='danger'>Delete</Button>
                         </div>
                     ))
                 }
